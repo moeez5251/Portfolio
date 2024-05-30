372fe1c12ae66687f0974e1c93059a2724daf0cb
Moeez <159162874+moeez5251@users.noreply.github.com>
2024-05-30 18:54:58 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,16 +4,51 @@ theme_icon.addEventListener('click', () => {
    theme_icon.classList.toggle('icon1');
    document.querySelector('body').classList.toggle('dark-theme');
 });
+
+// =======================>Navbar buttons events<=============================================
+
+let nav = document.getElementsByClassName('nav');
+nav[1].addEventListener('click', () => {
+   window.scrollTo(0, 1440);
+});
+nav[2].addEventListener('click', () => {
+   window.scrollTo(0, 600);
+});
+document.getElementById('btn2').addEventListener('click', () => {
+   window.scrollTo(0, 1440);
+
+});
+document.getElementById('btn1').addEventListener('click', () => {
+   window.scrollTo(0, 3332);
+
+});
+
 // =====================>Animation<================================
 var typed = new Typed('.auto-typed', {
-   strings: ["WEB-DEVELOPER", "UI-DESIGNER", "UIX-DESIGNER", "FREELANCER", "SERVER-TESTER", "APP-DEVELOPER", "CONTENT-WRITER", "VIDEO-EDITOR", "PROGRAMMER"],
+   strings: ["WEB-DEVELOPER", "UI/UIX-DESIGNER", "FREELANCER", "SERVER-TESTER", "APP-DEVELOPER", "CONTENT-WRITER", "VIDEO-EDITOR", "PROGRAMMER"],
    typeSpeed: 100,
    backSpeed: 20,
    loop: true
 });
+var typed1 = new Typed('.auto', {
+   strings: ["WEB-DEVELOPER", "UI/UIX-DESIGNER", "FREELANCER", "SERVER-TESTER", "APP-DEVELOPER", "CONTENT-WRITER", "VIDEO-EDITOR", "PROGRAMMER"],
+   typeSpeed: 80,
+   backSpeed: 80,
+   loop: true
+});
+
+let cursor=document.getElementsByClassName('typed-cursor');
+cursor[1].style.fontSize='24px';
+cursor[1].style.fontWeight='bold';
+
+// =====================>Default Window Scroll<================================
+
 window.scrollTo(0, 10);
+// =====================>Window event listner<=================================
+
 window.addEventListener('scroll', () => {
    var scroll = window.scrollY;
+// =====================>Event Listener for pic animation<=================================
    if (scroll <= 270 || scroll === 0) {
 
       setTimeout(() => {
@@ -21,17 +56,21 @@ window.addEventListener('scroll', () => {
          temp.classList.add('pic');
       }, 100);
    }
+// =====================>Contact form animation<=================================
+
    let inputs = document.getElementsByClassName('inputs');
-   if (window.scrollY > 2410) {
+   if (window.scrollY > 2560) {
       document.querySelector('.forms').style.left = '0px';
    };
-   if (window.scrollY > 1610) {
+// =====================>Timeline animation<=================================
+
+   if (window.scrollY > 1810) {
       document.querySelector('.my-skills').style.left = '0px';
    };
 
 });
-let section_3 = document.getElementsByClassName('section-3');
 
+// =====================>Scroll to top button event<=================================
 let scroll_button = document.querySelector('.goto-top');
 window.addEventListener('scroll', () => {
    var scr = window.scrollY;
@@ -48,6 +87,7 @@ window.addEventListener('scroll', () => {
    }
 
 });
+// =====================>Event listners for Timeline buttons<=================================
 let names = document.querySelectorAll('.names');
 let purple_color = document.querySelector('.purple-color');
 let time = document.querySelectorAll('.time');
@@ -71,7 +111,9 @@ names[0].addEventListener('click', () => {
    description[2].style.opacity='0';
    description[3].style.opacity='0';
 });
+//Default Click
 names[0].click();
+//2nd Button
 names[1].addEventListener('click', () => {
    names[1].style.color = '#a905ed';
    names[0].style.color = 'gray';
@@ -90,6 +132,7 @@ names[1].addEventListener('click', () => {
    description[2].style.opacity='0';
    description[3].style.opacity='0';
 });
+//3rd Button
 names[2].addEventListener('click', () => {
    names[2].style.color = '#a905ed';
    names[0].style.color = 'gray';
@@ -108,6 +151,7 @@ names[2].addEventListener('click', () => {
    description[0].style.opacity='0';
    description[3].style.opacity='0';
 });
+//4th Button
 names[3].addEventListener('click', () => {
    names[3].style.color = '#a905ed';
    names[0].style.color = 'gray';
@@ -126,27 +170,14 @@ names[3].addEventListener('click', () => {
    description[2].style.opacity='0';
    description[0].style.opacity='0';
 });
-
+// =====================>Project landspace images styling<=================================
 let image = document.getElementsByClassName('project-image');
 image[1].style.width = '40%'
 image[3].style.width = '40%'
 image[4].style.width = '40%'
 
-let nav = document.getElementsByClassName('nav');
-nav[1].addEventListener('click', () => {
-   window.scrollTo(0, 1440);
-});
-nav[2].addEventListener('click', () => {
-   window.scrollTo(0, 600);
-});
-document.getElementById('btn2').addEventListener('click', () => {
-   window.scrollTo(0, 1440);
 
-});
-document.getElementById('btn1').addEventListener('click', () => {
-   window.scrollTo(0, 3332);
-
-});
+// =====================>Contact form submission<=================================
 
 let inputs = document.querySelectorAll('.inputs');
 document.querySelector('form').addEventListener('submit', (event) => {
@@ -154,8 +185,8 @@ document.querySelector('form').addEventListener('submit', (event) => {
    let email = inputs[1].value;
    let phone = inputs[2].value;
    let Message = inputs[3].value;
-   event.preventDefault();
-
+   event.preventDefault();// For Preventing reload on submit
+// Mail sending
    emailjs.send("service_5fm08d6", "template_c8sru5i", {
       from_name: name,
       from_email: email,
@@ -174,6 +205,8 @@ document.querySelector('form').addEventListener('submit', (event) => {
    })
 
 });
+// =====================>For Contacting me social Medias Icons styling<=================================
+
 let logos = document.getElementsByClassName('logos');
 let names_of_logo = document.getElementsByClassName('name-of-logo');
 
@@ -189,7 +222,7 @@ for (let i = 0; i < 3; i++) {
    });
 
 }
-
+// =====================>HamBurger Menu<=================================
 
 let burger = document.querySelector('.burger');
 let options = document.querySelector('.options');
