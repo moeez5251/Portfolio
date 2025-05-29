db6e08011e7266b0689274ebd33394ceeef8407b
moeez5251 <moeez66656@gmail.com>
2025-05-29 14:33:47 +0500

    Adjust layout and styles for responsiveness across components 📱

--- src/App.jsx
+++ src/App.jsx
@@ -615,20 +615,24 @@ function App() {
             </a>
           </div>
           <div>
-            <a target="_blank" href="https://twitter.com/moeez5251">
+            <a target="_blank" href="https://x.com/MoeezSheik53288">
 
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width={24}
                 height={24}
                 fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                className="feather feather-twitter fea icon-sm"
+                className="injected-svg"
+                color="white"
+                data-src="https://cdn.hugeicons.com/icons/new-twitter-stroke-standard.svg"
               >
-                <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
+                <path
+                  stroke="white"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={1.5}
+                  d="m3 21 7.548-7.548M21 3l-7.548 7.548m0 0L8 3H3l7.548 10.452m2.904-2.904L21 21h-5l-5.452-7.548"
+                />
               </svg>
             </a>
           </div>
