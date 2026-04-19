fcd18843eb1c1b062258e854bdf0e6f46afdeb1e
moeez5251 <moeez4241@gmail.com>
2026-04-19 14:09:45 +0500

    feat: enhance contact form with error handling and dynamic email service integration

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,19 +3,30 @@ import "./contact.css"
 import { useForm } from "react-hook-form"
 import emailjs from '@emailjs/browser';
 const Contact = () => {
+    console.log(import.meta.env.VITE_EMAIL_JS_ID)
     const form = useRef();
     const [green, setgreen] = useState(false)
+    const [error, seterror] = useState(false)
     const {
         register,
         handleSubmit,
         formState: { errors },
     } = useForm()
 
-    const onSubmit = () => {
-        emailjs
-            .sendForm('service_4cldqmj', 'template_c8sru5i', form.current, {
-                publicKey: 'OsnOld423T6zK_QBV',
-            })
+    const onSubmit = async () => {
+        try {
+            await emailjs
+                .sendForm(import.meta.env.VITE_EMAIL_JS_ID, 'template_c8sru5i', form.current, {
+                    publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
+                })
+        } catch (e) {
+            console.log(e)
+            seterror(true)
+            setTimeout(() => {
+                seterror(false);
+            }, 2500);
+            return
+        }
         setgreen(true);
         setTimeout(() => {
             setgreen(false);
@@ -28,7 +39,7 @@ const Contact = () => {
 
                 <div className="left-contact">
                     <div className="box">
-                        <img data-aos="zoom-in" data-aos-duration="500" loading='lazy'  src={`${import.meta.env.BASE_URL}assets/contact.webp`} alt="Contact Photo" />
+                        <img data-aos="zoom-in" data-aos-duration="500" loading='lazy' src={`${import.meta.env.BASE_URL}assets/contact.webp`} alt="Contact Photo" />
                         <p data-aos="zoom-in" data-aos-duration="500" >I'm also available for freelance work. Get in touch right now.</p>
                         <p data-aos="zoom-in" data-aos-duration="500 "><span>Phone: </span>03207349527</p>
                         <p data-aos="zoom-in" data-aos-duration="500" ><span>Email: </span>moeez4241@gmail.com</p>
@@ -42,17 +53,18 @@ const Contact = () => {
                                 <span className="red">Please Enter Your Name</span>
                             ) : errors.email ? (
                                 <span className="red">Pleas Enter Your email</span>
-                            ) : errors.subject ? (<span className="red">Please Enter Your Subject</span>) : ""}
+                            ) : errors.subject ? (<span className="red">Please Enter Your Subject</span>) : errors.text ? (<span className="red">Please Enter Your Message</span>) : null}
                             {green && <span className='green'>Message sent !. Contact You soon 😊</span>}
+                            {error && <span style={{ color: "red" }}>Error in Sending Message</span>}
                         </div>
-                        <input data-aos="zoom-in" data-aos-duration="500"  className='inputs-form' name='person' type="text" placeholder='Name:' {...register("person", { required: true })} />
-                        <input data-aos="zoom-in" data-aos-duration="500"  className='inputs-form' name='email' type="email" placeholder='Email: ' {...register("email", { required: true })} />
-                        <input data-aos="zoom-in" data-aos-duration="500"  className='inputs-form' name='subject' type="text" placeholder='Subject' {...register("subject", { required: true })} />
-                        <textarea data-aos="zoom-in" data-aos-duration="500"  className='inputs-form' placeholder='Message :' rows={5} cols={5} name="text"  {...register("text", { required: true })} ></textarea>
-                        <button data-aos="zoom-in" data-aos-duration="500"  className='btn-contact' type='submit'>
+                        <input data-aos="zoom-in" data-aos-duration="500" className='inputs-form' name='person' type="text" placeholder='Name:' {...register("person", { required: true })} />
+                        <input data-aos="zoom-in" data-aos-duration="500" className='inputs-form' name='email' type="email" placeholder='Email: ' {...register("email", { required: true })} />
+                        <input data-aos="zoom-in" data-aos-duration="500" className='inputs-form' name='subject' type="text" placeholder='Subject' {...register("subject", { required: true })} />
+                        <textarea data-aos="zoom-in" data-aos-duration="500" className='inputs-form' placeholder='Message :' rows={5} cols={5} name="text"  {...register("text", { required: true })} ></textarea>
+                        <button data-aos="zoom-in" data-aos-duration="500" className='btn-contact' type='submit'>
                             Send <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill='white'>
-                            <path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480v-83.6c0-4 1.5-7.8 4.2-10.8l167.6-182.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8l-88.3-44.2C7.1 311.3.3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z" />
-                        </svg></button>
+                                <path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480v-83.6c0-4 1.5-7.8 4.2-10.8l167.6-182.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8l-88.3-44.2C7.1 311.3.3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z" />
+                            </svg></button>
                     </form>
                 </div>
             </div>
