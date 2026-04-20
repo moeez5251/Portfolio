76e3417345416027358c26228aaef7f6c1ddd81a
moeez5251 <moeez4241@gmail.com>
2026-04-20 23:31:11 +0500

    feat: enhance theme toggle functionality with SVG icons and improved accessibility

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -64,10 +64,42 @@ const Navbar = () => {
           <li><a className="navbar-li" href="#contact">CONTACT</a></li>
         </ul>
       </div>
-      <label className="switch theme-toggle-btn">
-        <input checked={isDarkMode}  onChange={toggleTheme} type="checkbox" />
-        <span className="slider"></span>
+      <label
+        title={isDarkMode ? "Light Mode" : "Dark Mode"}
+        htmlFor="themeToggle"
+        className="themeToggle st-sunMoonThemeToggleBtn theme-toggle-btn"
+        type="checkbox"
+      >
+        <input checked={isDarkMode} onChange={toggleTheme} type="checkbox" id="themeToggle" className="themeToggleInput" />
+        <svg
+          width="18"
+          height="18"
+          viewBox="0 0 20 20"
+          fill="white"
+          stroke="none"
+        >
+          <mask id="moon-mask">
+            <rect x="0" y="0" width="20" height="20" fill="white"></rect>
+            <circle cx="11" cy="3" r="8" fill="black"></circle>
+          </mask>
+          <circle
+            className="sunMoon"
+            cx="10"
+            cy="10"
+            r="8"
+            mask="url(#moon-mask)"
+          ></circle>
+          <g>
+            <circle className="sunRay sunRay1" cx="18" cy="10" r="1.5"></circle>
+            <circle className="sunRay sunRay2" cx="14" cy="16.928" r="1.5"></circle>
+            <circle className="sunRay sunRay3" cx="6" cy="16.928" r="1.5"></circle>
+            <circle className="sunRay sunRay4" cx="2" cy="10" r="1.5"></circle>
+            <circle className="sunRay sunRay5" cx="6" cy="3.1718" r="1.5"></circle>
+            <circle className="sunRay sunRay6" cx="14" cy="3.1718" r="1.5"></circle>
+          </g>
+        </svg>
       </label>
+
     </nav>
   );
 };
